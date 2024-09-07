c10d7917ed326cbbd29e9dadf8846d894ec25a82
Delfi Lh <delfinalasheras@gmail.com>
2024-09-07 17:20:32 -0300

    aaalas credenciales

--- repository/healthybite/src/App.js
+++ repository/healthybite/src/App.js
@@ -9,25 +9,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABXtMyR7Fi-xshZaVaelZMwkAldt4WB0M",
-  authDomain: "healthybite-b2a20.firebaseapp.com",
-  databaseURL: "https://healthybite-b2a20-default-rtdb.firebaseio.com",
-  projectId: "healthybite-b2a20",
-  storageBucket: "healthybite-b2a20.appspot.com",
-  messagingSenderId: "1061070227538",
-  appId: "1:1061070227538:web:7c622ae4edd5d0e68ff78b",
-  measurementId: "G-K873CFX9CS"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-// }
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const firestore =getFirestore(app)
