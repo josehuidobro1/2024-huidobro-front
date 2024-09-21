63685ff24b0ba0efaa59225abc36f25e39da1ae0
jhuido <josefinahuidobro@uca.edu.ar>
2024-09-21 11:16:45 -0300

    SCRUM-41: https://healthybite.atlassian.net/browse/SCRUM-41

--- repository/healthybite/src/App.js
+++ repository/healthybite/src/App.js
@@ -3,13 +3,21 @@ import ResetPassword from './pages/ResetPassword/ResetPassword';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebaseConfig';
 import Home from './pages/Home/Home';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [user]= useAuthState(auth)
   return (
-    <div>
-      {user? <Home/>: <Login/>}
-    </div>
+    <Router>
+        <Routes>
+          { user? <Route path="/" element={<Home />} /> : <Route path="/login" element={<Login />} />}
+          {/*<Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/user-profile" element={<UserProfile />} />
+          <Route path="/category" element={<Category />} />*/}
+        </Routes>
+      </Router>
+
   );
 }
 
