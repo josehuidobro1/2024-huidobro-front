c4c22a5fc554dda16dd69148d7b9a8d80566501f
jhuido <josefinahuidobro@uca.edu.ar>
2024-10-03 12:24:22 -0300

    SCRUM-59: https://healthybite.atlassian.net/browse/SCRUM-59

--- repository/healthybite/src/components/NavBar.js
+++ repository/healthybite/src/components/NavBar.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faLayerGroup, faChartLine, faUser, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'; 
 import React, { useEffect, useState } from "react";
+import NavItem from "./NavItem";
 
 function NavBar() {
     const [openBar, setOpenBar]=useState(false)
 
     const handleLogOut=()=>{
         auth.signOut()
+        setOpenBar(false)
 
     }
     return (
@@ -18,11 +20,11 @@ function NavBar() {
                 <h1 className="font-belleza text-2xl lg:text-4xl text-white  ">Healthy Bite</h1>
                 {window.innerWidth > '768' ? 
                 <>
-                    <Link to="/" className="text-white font-quicksand text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " > <FontAwesomeIcon className="text-white text-md pr-2" icon={faHouse} />Home </Link>
-                    <Link to="/category" className="text-white font-quicksand text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faLayerGroup} />Category </Link>
-                    <Link to="/dashboard" className="text-white font-quicksand text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faChartLine} />Dashboard </Link>
-                    <Link to="/user-profile" className="text-white font-quicksand text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faUser} />User Profile </Link>
-                    <Link to="/"  onClick={()=>auth.signOut()} className="font-quicksand font-semibold text-sm text-healthyGreen hover:mt-1  px-3 py-1 rounded-3xl bg-white hover:bg-hbGreen  underline-offset-2">Log out</Link>
+                    <NavItem route='/' name='Home' icon={faHouse} setOpenBar={setOpenBar} />
+                    <Link  to="/category" className="text-white font-quicksand text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faLayerGroup} />Category </Link>
+                    <Link  to="/dashboard" className="text-white font-quicksand text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faChartLine} />Dashboard </Link>
+                    <Link  to="/user-profile" className="text-white font-quicksand text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faUser} />User Profile </Link>
+                    <Link  to="/"  onClick={()=>auth.signOut()} className="font-quicksand font-semibold text-sm text-healthyGreen hover:mt-1  px-3 py-1 rounded-3xl bg-white hover:bg-hbGreen  underline-offset-2">Log out</Link>
                 </>
                 :
                 <div>
@@ -34,11 +36,11 @@ function NavBar() {
             { openBar &&
                     <div className="w-full  xs:absolute z-10 right-0  xs:top-16    flex items-center justify-center">
                         <div className="bg-healthyGreen xs:shadow-lg w-11/12 h-screen xs:h-full sm:w-10/12 pb-12 xs:pb-2 py-2  px-3 xs:rounded-3xl flex flex-col xs:flex-row  items-center justify-around ">
-                            <Link to="/" className="text-white font-quicksand text-xl xs:text-xs hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " > <FontAwesomeIcon className="text-white text-md pr-2" icon={faHouse} />Home </Link>
-                            <Link to="/" className="text-white font-quicksand text-xl xs:text-xs hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faLayerGroup} />Category </Link>
-                            <Link to="/" className="text-white font-quicksand text-xl xs:text-xs hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faChartLine} />Dashboard </Link>
-                            <Link to="/user-profile" className="text-white font-quicksand text-xl xs:text-xs hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " ><FontAwesomeIcon className="text-white text-md pr-2" icon={faUser} />User Profile </Link>
-                            <Link to="/" onClick={()=>auth.signOut()} className="font-quicksand font-semibold text-xl xs:text-sm text-healthyGreen hover:mt-1  px-4 py-2 xs:px-3 xs:py-1 rounded-3xl bg-white hover:bg-hbGreen  underline-offset-2">Log out</Link>
+                            <NavItem route='/' name='Home' icon={faHouse} setOpenBar={setOpenBar} />
+                            <NavItem route='/dashboard' name='Dashboard' icon={faChartLine} setOpenBar={setOpenBar} />
+                            <NavItem route='/category' name='Category' icon={faLayerGroup} setOpenBar={setOpenBar} />
+                            <NavItem route='/user-profile' name='User Profile' icon={faUser} setOpenBar={setOpenBar} />
+                            <Link to="/" onClick={handleLogOut} className="font-quicksand font-semibold text-xl xs:text-sm text-healthyGreen hover:mt-1  px-4 py-2 xs:px-3 xs:py-1 rounded-3xl bg-white hover:bg-hbGreen  underline-offset-2">Log out</Link>
                         </div>
                     </div>
 
---
+++ repository/healthybite/src/components/NavItem.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const NavItem = ({route, name, icon, setOpenBar}) => {
+  return (
+    <>
+        {window.innerWidth > '768' ? 
+            <Link  to={route} className="text-white font-quicksand  text-sm hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " > 
+                <FontAwesomeIcon className="text-white text-md pr-2" icon={icon} />
+                <p>{name}</p>
+            </Link>
+        :
+            <Link onClick={()=>setOpenBar(false)} to={route} className="text-white flex items-center justify-between w-3/4 py-2 px-4 rounded-lg border-2 border-white font-quicksand text-xl xs:text-xs hover:mt-1 hover:underline-offset-2 hover:border-b-1 hover:border-b-white  " > 
+                <FontAwesomeIcon className="text-white text-md pr-2  w-1/5" icon={icon} />
+                <p className='w-4/5 text-left '>{name}</p>
+            </Link>
+        }
+
+    </>
+  )
+}
+
+export default NavItem
