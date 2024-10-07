ac3617f4ade805750b000fca0bce5cb3b1ce8830
jhuido <josefinahuidobro@uca.edu.ar>
2024-10-07 14:10:38 -0300

    SCRUM-67: https://healthybite.atlassian.net/browse/SCRUM-67

--- repository/healthybite/src/pages/Category/components/NewCategory.js
+++ repository/healthybite/src/pages/Category/components/NewCategory.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState }  from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpRightAndDownLeftFromCenter} from '@fortawesome/free-solid-svg-icons'; 
 import SaveButton from './SaveButton';
-import Data from '../../Data'
+import Data from '../../Data';
+import noodle from '../../../assets/icon.png'
 import { createCategory, fetchAllFoods } from '../../../firebaseService';
 
 function NewCategory({handleUpdate, setAddCategory, setAddFood, foods}) {
@@ -11,6 +12,7 @@ function NewCategory({handleUpdate, setAddCategory, setAddFood, foods}) {
     const iconOptions = Data.iconOptions
     const [iconSelected,setIconSelected]=useState(null)
     const [message, setMessage]=useState('')
+    const [addIcon, setAddIcon]=useState(false)
 
     const handleIcon=(icon)=>{
         setIconSelected(icon)
@@ -70,18 +72,20 @@ function NewCategory({handleUpdate, setAddCategory, setAddFood, foods}) {
             <p className='py-1 px-4 rounded-2xl bg-healthyDarkOrange text-white font-quicksand font-bold text-sm text-center'>{message}</p>
         </div>}
         <div className='flex flex-row w-full items-center justify-between'>
-            <p className='font-semibold text-white text-xs lg:text-sm mr-2 '>Name</p>
-            <input value={name} onChange={(e)=>setName(e.target.value)} className='bg-white rounded-md text-darkGray text-sm p-1 w-full' />
-        </div>
-        <div className='flex flex-row w-full items-center justify-center mt-3'>
-            <p className={`w-1/5 font-semibold text-white text-xs lg:text-sm mr-2 `}>Icon</p>
-            <div className={`${ iconSelected ?  'w-3/5' : 'w-4/5' }  flex justify-end items-start `}>
-                <div className='shadow-lg bg-white p-1 rounded-md mb-1 flex flex-row items-center justify-start overflow-x-auto'>
+            <div className={`flex flex-col justify-center ${ addIcon  ? 'w-3/4' : 'w-2/3'} `}>
+                <input value={name} onChange={(e)=>setName(e.target.value)} placeholder='Category name' className='bg-white rounded-md text-center text-darkGray text-sm p-1 focus:outline-none  focus:ring-2 focus:ring-healthyGreen' />
+                {addIcon && <div className='shadow-lg mt-1 bg-white p-1 rounded-md mb-1 flex flex-row items-center justify-start overflow-x-auto'>
                     {iconOptions.map((item, index)=>
                         <FontAwesomeIcon key={index} onClick={()=>handleIcon(item)} icon={item.icon} className='text-xl text-healthyGray1 mt-2 mx-2 hover:cursor-pointer  hover:text-healthyDarkGray1' />)}
-                </div>
+                </div>}
             </div>
-            {iconSelected && <FontAwesomeIcon icon={iconSelected.icon} className='w-1/5 text-white text-2xl'/>}
+            
+            {iconSelected ? <FontAwesomeIcon icon={iconSelected.icon} className='w-full text-white text-3xl py-2 mx-2 px-1 rounded-full border-2 border-white'/>
+            : 
+            <button onClick={()=>setAddIcon(true)} className={`${ addIcon ?'w-1/4' :'w-1/3'} mx-2 flex justify-center items-center py-2 px-1 rounded-full border-2 border-white hover:bg-white`}>
+                <img src={noodle} alt="icon" className='w-10/12'  />
+                <span className="absolute text-5xl font-semibold text-healthyDarkGreen p-4 opacity-0 hover:opacity-100">+</span>
+            </button>}
         </div>
         <div className='flex flex-col w-full mt-3 '>
             <div className='w-full flex flex-row items-center justify-between mb-2'>
