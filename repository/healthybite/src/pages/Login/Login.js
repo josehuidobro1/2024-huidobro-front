37b0e88b3d4723e06a0e33ce78e7760bf63ee3f9
jhuido <josefinahuidobro@uca.edu.ar>
2025-02-17 13:09:48 -0300

    manifiest, y url

--- repository/healthybite/src/pages/Login/Login.js
+++ repository/healthybite/src/pages/Login/Login.js
@@ -52,7 +52,7 @@ function Login() {
         e.preventDefault();
         try {
             const actionCodeSettings = {
-                url: "https://2024-huidobro-front.vercel.app", 
+                url: "https://2024-huidobro-front-ey08brtzo-josehuidobro1s-projects.vercel.app/resetPassword/", 
                 handleCodeInApp: true, 
             };
             await sendPasswordResetEmail(auth, email, actionCodeSettings);
